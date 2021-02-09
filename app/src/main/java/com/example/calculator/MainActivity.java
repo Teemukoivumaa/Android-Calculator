2dc8642c19eed5f0f46451bb5df306197bee1b4e
Teemu Koivumaa <35847478+Teemukoivumaa@users.noreply.github.com>
2021-02-09 13:19:41 +0200

    special marks
    Added special mark adding to calculation and started percent calculation

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -29,32 +29,85 @@ public class MainActivity extends AppCompatActivity {
         Button b = (Button)view;
         String buttonText = b.getText().toString();
 
-        Log.i("Test", buttonText);
         valueText = valueText + buttonText;
         inputValues.setText(valueText);
     }
 
     public void specialMarkers(View view) {
-        String valueText = inputValues.getText().toString();
+        String calculation = inputValues.getText().toString();
+        String newCalculation = "";
 
         Button b = (Button)view;
         String buttonText = b.getText().toString();
 
         switch (buttonText) {
             case "C":
-                valueText = ""; inputValues.setText(valueText);
+                newCalculation = ""; inputValues.setText(newCalculation);
                 break;
             case "Erase":
-                if (!valueText.isEmpty()) { valueText = removeLastChar(valueText); inputValues.setText(valueText); }
+                if (!calculation.isEmpty()) { newCalculation = removeLastChar(calculation); inputValues.setText(newCalculation); }
+                break;
+            case "%":
+                if (!isPreviousMarkSpecial(calculation)) {
+                    newCalculation = presentageOf(calculation);
+                    inputValues.setText(newCalculation);
+                }
+                break;
+            case "+":
+            case "-":
+            case "÷":
+            case "x":
+                if (isPreviousMarkSpecial(calculation)) {
+                    calculation = removeLastChar(calculation);
+                }
+                newCalculation = calculation + buttonText;
+                inputValues.setText(newCalculation);
                 break;
             default:
                 Toast.makeText(MainActivity.this, "Unknown value.", Toast.LENGTH_LONG).show();
         }
+    }
 
-        inputValues.setText(valueText);
+    private String presentageOf(String calculation) {
+        float percent = 0;
+        int stopLocation = 0;
+        StringBuilder precentageCalculation = new StringBuilder();
+
+        for (int i = calculation.length() - 1; i >= 0; i--) {
+            if (Character.isDigit(calculation.charAt(i))) {
+                Log.i("Numerot", String.valueOf(calculation.charAt(i)));
+                precentageCalculation.insert(0, calculation.charAt(i));
+            } else { stopLocation = i; i = 0; }
+        }
+
+        float nums = Integer.parseInt(String.valueOf(precentageCalculation));
+        percent = nums / 100;
+
+        String newCalculation = calculation.substring(0, calculation.length() - stopLocation) + percent;
+
+        return newCalculation;
+    }
+
+    private Boolean isPreviousMarkSpecial(String calculation) {
+        Boolean previousSpecial = false;
+        String lastChar = calculation.substring(calculation.length() - 1);
+        Toast.makeText(MainActivity.this, lastChar, Toast.LENGTH_LONG).show();
+        if (lastChar.equals("+") || lastChar.equals("-") || lastChar.equals("÷") || lastChar.equals("x")) {
+            previousSpecial = true;
+        }
+        return previousSpecial;
     }
 
     private String removeLastChar(String str) {
         return str.substring(0, str.length() - 1);
     }
+
+     /*
+        for (int i = 0; i < calculation.length(); i++) {
+            System.out.println(calculation.charAt(i));
+            if (Character.isLetter(calculation.charAt(i))) {
+                lastLetterLocation = i;
+            }
+        }
+        */
 }
